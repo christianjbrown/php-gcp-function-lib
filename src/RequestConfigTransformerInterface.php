4332c35f94c9cab7ba641acbcaa2c1dd04f47e44
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-09-24 17:57:00 +0100

    Add RequestConfigTransformer

---
+++ src/RequestConfigTransformerInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChristianBrown\CloudFunction;
+
+interface RequestConfigTransformerInterface
+{
+    public const ENV_DEBUG = 'DEBUG';
+    public const ENV_K_REVISION = 'K_REVISION';
+    public const ENV_REQUIRED_HEADER_KEY = 'REQUIRED_HEADER_KEY';
+    public const ENV_REQUIRED_HEADER_VALUE = 'REQUIRED_HEADER_VALUE';
+    public const ENV_REQUIRED_ORIGIN = 'REQUIRED_ORIGIN';
+
+    public function transform(array $env): RequestConfigInterface;
+}
